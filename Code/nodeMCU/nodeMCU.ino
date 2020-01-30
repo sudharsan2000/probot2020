b9d655ac160a323937171f10eaac3b3bbd2832ec
NithinEiswar <nit141100@gmail.com>
2020-01-30 18:03:20 +0530

    Comments added

--- Code/nodeMCU/nodeMCU.ino
+++ Code/nodeMCU/nodeMCU.ino
@@ -11,40 +11,42 @@ ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
 Servo servo;  //creating an object of class servo
 String page = "<html> \n <body>";
 void setup(void){
-  servo.attach(D2);
-  Serial.begin(115200);
+  servo.attach(D2);//assigning pin D2 to input pin of servo motor
+  Serial.begin(115200);//Begins the serial communication at baud rate=115200(bits per second)
   WiFi.begin(ssid, password); //begin WiFi connection
   Serial.println("");
  
-  // Wait for connection
+  // Wait for connection to the hotspot
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  digitalWrite(LED_BUILTIN,HIGH);
+  digitalWrite(LED_BUILTIN,HIGH);//the LED_BUILTIN in NodeMCU is made high
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());//the IP address of the network to which it is connected is printed on serial monitor
   server.on("/turnoff",turnoff);
   server.on("/turnon",turnon);
-  server.begin();
+  server.begin();//Starting the web server
   Serial.println("Web server started!");
   
 }
-//Function to rotate the servo motor to a specific position so that closing of door latch takes place
+//Function to rotate the servo motor to a specific position so that one movement of door latch takes place
  void turnoff()
  {
    Serial.println(0);//printing 0 on serial monitor
    servo.write(0);//this rotates the servo to zero degree position
-   server.send(200,"text/html","<html> <h1> off</h1> </html>");//printing off on the webpage created
+   server.send(200,"text/html","<html> <h1> off</h1> </html>");//printing off in the webpage created
  }
+
+//Function to rotate the servo motor to a specific position so that opposite movement of door latch takes place(as compared to turnoff) 
   void turnon()
  {
-   Serial.println(1);
-   servo.write(90);
-   server.send(200,"text/html","<html> <h1> on</h1> </html>");
+   Serial.println(1);//printing 1 on serial monitor
+   servo.write(90);//this rotates the servo to ninety degree position
+   server.send(200,"text/html","<html> <h1> on</h1> </html>");//printing on in the webpage created
  }
 void loop(void)
 {
@@ -54,5 +56,5 @@ void loop(void)
     Serial.print(".");
   }
 
-  server.handleClient();
+  server.handleClient();// to handle the actual incoming of HTTP requests, we need to call the handleClient method on the server object
   }
